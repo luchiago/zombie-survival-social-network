bcec4d2d44f53238401654dbe1f493086940352c
luchiago <lucashiago63@gmail.com>
2019-01-06 20:39:03 +0000

    new virtualenv added

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/luc/Desktop/Workspace Python/Trainee_Code/ZSSN/zombie-survival-social-network/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
