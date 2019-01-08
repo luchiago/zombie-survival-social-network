a9c7e0002f05f4f86b32d025172cda96a53c9d82
luchiago <lucashiago63@gmail.com>
2019-01-08 12:39:52 +0000

    env deleted

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/luc/Desktop/Workspace Python/Trainee_Code/zombie-survival-social-network/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
